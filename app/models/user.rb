e394f1c65aaa68fa13abdb4057063cac2c95de34
Fernando G <kev9504@gmail.com>
2019-01-24 02:32:30 +0000

    added validation to user first name and last name model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates_presence_of :first_name, :last_name
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "user creation" do
+      before do
+          @user=User.create(
+              first_name:'John', 
+              last_name:'Doe',
+              email:'some@email.com',
+              password:'secret',
+              password_confirmation:'secret'
+              )
+      end
+      
+      it "should be created" do 
+          expect(@user).to be_valid
+      end
+      
+      it "should validate presence of first and last name" do 
+          @user.first_name=nil
+          @user.last_name=nil
+          expect(@user).to be_invalid
+      end
+  end
 end
