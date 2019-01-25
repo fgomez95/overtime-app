3fafd15b070037dd9a03720910b6a9d28dae80f2
Fernando G <kev9504@gmail.com>
2019-01-25 00:09:43 +0000

    refactor code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :edit, :delete, :update]
+    
     def index
-        @posts=Post.all
+        @posts = Post.all
     end
     
     def new 
@@ -8,18 +10,24 @@ class PostsController < ApplicationController
     end
     
     def create 
-        @post=Post.new(post_params)
+        @post = Post.new(post_params)
         if @post.save
-            redirect_to post_path(@post)
+            redirect_to post_path(@post), notice: 'your post has been created'
+        else 
+            render :new
         end
     end
     
     def show 
-        @post=Post.find(params[:id])
     end
     
     private
+    
     def post_params
         params.require(:post).permit(:date, :rationale)
+    end
+    
+    def set_post
+        @post=Post.find(params[:id])
     end
 end
