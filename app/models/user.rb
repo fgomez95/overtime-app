c7f245d18eed88f33401ee32672d7b32dacb76f3
Fernando G <kev9504@gmail.com>
2019-01-28 21:07:33 +0000

    implemented full name for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   validates_presence_of :first_name, :last_name
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  def full_name
+    "#{last_name}, #{first_name}"
+  end
 end
