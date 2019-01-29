69320c6ecb563f472ec4171e587ed69121d7f476
Fernando G <kev9504@gmail.com>
2019-01-29 14:59:59 +0000

    implemented new post link on the navigation bar

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -25,6 +25,14 @@ describe 'navigate' do
         end
     end
     
+    describe 'new' do 
+        it 'has a link from the homepage' do 
+            visit root_path
+            click_link("new-post-link")
+            expect(page.status_code).to be(200)
+        end
+    end
+    
     describe 'creation' do 
         before do 
             visit new_post_path
