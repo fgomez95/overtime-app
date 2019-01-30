9666bab7d5f85a6c7c6e5524ead274a74309070c
Fernando G <kev9504@gmail.com>
2019-01-30 12:27:47 +0000

    fixed FactoryBot deprecation warnings

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,18 +4,18 @@ FactoryBot.define do
   end
   
   factory :user do
-    first_name 'John'
-    last_name 'Doe'
+    first_name { 'John' }
+    last_name { 'Doe' }
     email { generate :email }
-    password 'secret'
-    password_confirmation 'secret'
+    password { 'secret' }
+    password_confirmation { 'secret' }
   end
   
   factory :second_user, class: 'AdminUser' do
-    first_name 'D'
-    last_name 'Admin'
+    first_name { 'D' }
+    last_name { 'Admin' }
     email { generate :email }
-    password 'secret'
-    password_confirmation 'secret'
+    password { 'secret' }
+    password_confirmation { 'secret' }
   end
 end
