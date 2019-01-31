32f2ac0faa0255002b3f97acd066529828dbc332
Fernando G <kev9504@gmail.com>
2019-01-31 14:39:51 +0000

    implemented conditional to render status form

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -11,11 +11,19 @@ describe 'navigate' do
             @post = FactoryBot.create(:post)
         end
         
-        it 'has a status that can be edited on the form' do 
+        it 'has a status that can be edited on the form by the admin' do 
             visit edit_post_path(@post)
             page.choose('post_status_approved')
             click_on "Save"
             expect(@post.reload.approved?).to be(true)
+        end
+        
+        it 'cannot be edited by a non admin' do 
+            logout(@admin_user)
+            @user=FactoryBot.create(:user)
+            login_as(@user, :scope => :user)
+            visit edit_post_path(@post)
+            expect(page).to_not have_content('Approved')
         end
     end
 end
