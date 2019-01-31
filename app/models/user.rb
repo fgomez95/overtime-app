8d7cdf736f221926a049f9dd3f6075f52690104d
Fernando G <kev9504@gmail.com>
2019-01-31 13:13:39 +0000

    implemented enum status for posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates_presence_of :first_name, :last_name
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+  
   def full_name
     "#{last_name}, #{first_name}"
   end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,24 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  describe "Post creation" do 
+  describe 'Post creation' do 
     before do
       @user = FactoryBot.create(:user)
       @post = FactoryBot.create(:post)
     end
       
-    it "should be created" do 
+    it 'should be created' do 
         expect(@post).to be_valid
     end
     
-    it "should validate presence of date and rationale" do 
+    it 'should validate presence of date and rationale' do 
         @post.rationale=nil
         @post.date=nil
         expect(@post).to be_invalid
     end
     
-    it "should be associated with the user" do 
+    it 'should be associated with the user' do 
       expect(@post.user_id).to eq(User.last.id)
+    end
+    
+    it 'should have a default status of 0 when created' do 
+      expect(@post.read_attribute_before_type_cast(:status)).to eq(0)
+    end
+    
+    it 'should display a status as enum' do 
+      expect(@post.submitted?).to be(true)
     end
   end
 end
