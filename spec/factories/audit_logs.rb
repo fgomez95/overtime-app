73f665ee24a0779e2f0e79cffe90fb4416b9f9eb
Fernando Gomez <fgomez.sierra95@gmail.com>
2019-02-09 19:33:02 +0000

    git diff spec/models/audit_log_spec.rb

--- spec/factories/audit_logs.rb
+++ spec/factories/audit_logs.rb
@@ -1,8 +1,15 @@
 FactoryBot.define do
   factory :audit_log do
-    user { nil }
+    user
     status { 1 }
-    start_date { "2019-02-09" }
-    end_date { "2019-02-09" }
+    start_date { Date.today - 6.days }
+    end_date { "2019-02-20" }
+  end
+  
+  factory :second_audit_log, class: 'ActivityLog' do
+    second_user
+    status { 0 }
+    start_date { Date.today - 6.days }
+    end_date { "2025-12-9" }
   end
 end
--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe AuditLog, type: :model do
-
+    describe 'creation' do 
+        it 'can be created' do 
+            @audit_log = FactoryBot.create(:audit_log)
+            expect(@audit_log).to be_valid
+        end
+    end
 end
