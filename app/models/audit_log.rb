84ca5d1aef8fe2008f67e2f810add607672523e8
Fernando Gomez <fgomez.sierra95@gmail.com>
2019-02-10 19:58:54 +0000

    integrated validation and defaults for audit logs

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,3 +1,10 @@
 class AuditLog < ApplicationRecord
   belongs_to :user
+  validates_presence_of :status, :start_date
+  
+  after_initialize :set_defaults
+  
+  def set_defaults
+    self.start_date ||= Date.today - 6.days
+  end
 end
