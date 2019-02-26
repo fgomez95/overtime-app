59c01dd886adf79bfc174b19d83219e17c3503f2
Fernando Gomez <fgomez.sierra95@gmail.com>
2019-02-26 00:54:51 +0000

    implemented callback to update audit log when sent post

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,13 @@ class Post < ApplicationRecord
         rejected: 2,
     }
     scope :user_posts, -> (user){where(user_id: user.id)}
+    after_save :update_audit_log
+    
+    private
+    
+    def update_audit_log
+        audit_log = AuditLog.where(user_id: self.user_id, 
+        start_date: (self.date - 7.days..self.date)).last
+        audit_log.confirmed!
+    end
 end
